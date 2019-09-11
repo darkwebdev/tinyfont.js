4b2d54ac0dfd079ced7df610d31a1218199f73fa
Timur Manyanov <gtibalt@gmail.com>
2019-09-11 11:32:16 +0200

    Optimize fonts logic (bundle < 800b)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const DEFAULT_CHAR_HEIGHT = 5;
 const DEFAULT_COLOR = '#000';
 
-const bin2arr = (bin, width) => bin.match(new RegExp(`.{${width}}`, 'g'));
-
 export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
     if (!chars) {
         console.error('No font provided!');
@@ -10,16 +8,17 @@ export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
     }
     if (!ctx) {
         console.error('No context provided');
+        return
     }
+
+    const bin2arr = (bin, width) => bin.match(new RegExp(`.{${width}}`, 'g'));
+    const isNumber = code => code > 0;
 
     return (string, x=0, y=0, size=24, color=DEFAULT_COLOR) => {
         const renderChar = (charX, char) => {
             const pixelSize = size/height;
-            const binaryChar = chars[char];
-
-            if (!binaryChar) {
-                console.warn(`Char ${char} not found in the provided font`)
-            }
+            const fontCode = chars[char.charCodeAt()] || '';
+            const binaryChar = isNumber(fontCode) ? fontCode : fontCode.codePointAt();
 
             const binary = (binaryChar || 0).toString(2);
 
@@ -28,10 +27,10 @@ export const initFont = ({ height=DEFAULT_CHAR_HEIGHT, ...chars }={}, ctx) => {
             const formattedBinary = binary.padStart(width * height, 0);
             const binaryCols = bin2arr(formattedBinary, height);
 
-            console.debug('Rendering char', char, binaryCols);
+            console.debug('Rendering char', char, char.charCodeAt(), fontCode, binaryChar, binaryCols);
 
             binaryCols.map((column, colPos) =>
-                [...column].forEach((pixel, pixPos) => {
+                [...column].map((pixel, pixPos) => {
                     ctx.fillStyle = !+pixel ? 'transparent' : color; // pixel == 0 ?
                     ctx.fillRect(marginX + colPos * pixelSize, y + pixPos * pixelSize, pixelSize, pixelSize);
                 })
